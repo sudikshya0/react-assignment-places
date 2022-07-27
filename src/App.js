c67022a82bc317b174c276330fee08867106e474
sudikshya0 <sudikshya.rajkarnikar22@gmail.com>
2022-07-27 09:47:58 +0545

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import PersonList from './components/Listofname';
+
+function App() {
+  return (
+    <div className="App">
+    <PersonList/>
+    </div>
+  )
+}
+
+export default App;
